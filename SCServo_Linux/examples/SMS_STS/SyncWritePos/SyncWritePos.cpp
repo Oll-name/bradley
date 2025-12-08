7afc3c8929911b0baa1cb3aca5a6e1c15224579f
Ollie <o.bennett1@lancaster.ac.uk>
2025-12-08 10:25:27 +0000

    Changed SyncWritePos to testing code

--- SCServo_Linux/examples/SMS_STS/SyncWritePos/SyncWritePos.cpp
+++ SCServo_Linux/examples/SMS_STS/SyncWritePos/SyncWritePos.cpp
@@ -1,16 +1,8 @@
-/*
-舵机出厂速度单位是0.0146rpm，速度改为V=2400
-*/
-
 #include <iostream>
+#include <fstream>
 #include "SCServo.h"
 
 SMS_STS sm_st;
-
-u8 ID[2] = {1, 2};
-s16 Position[2];
-u16 Speed[2] = {2400, 2400};
-u8 ACC[2] = {50, 50};
 
 int main(int argc, char **argv)
 {
@@ -23,20 +15,35 @@ int main(int argc, char **argv)
         std::cout<<"Failed to init sms/sts motor!"<<std::endl;
         return 0;
     }
-	while(1){
-		Position[0] = 4095;
-		Position[1] = 4095;
-		sm_st.SyncWritePosEx(ID, sizeof(ID), Position, Speed, ACC);//舵机(ID1/ID2)以最高速度V=2400(步/秒)，加速度A=50(50*100步/秒^2)，运行至P1=4095位置
-		std::cout<<"pos = "<<4095<<std::endl;
-		usleep(2187*1000);//[(P1-P0)/V]*1000+[V/(A*100)]*1000
-  
-		Position[0] = 0;
-		Position[1] = 0;
-		sm_st.SyncWritePosEx(ID, sizeof(ID), Position, Speed, ACC);//舵机(ID1/ID2)以最高速度V=2400(步/秒)，加速度A=50(50*100步/秒^2)，运行至P0=0位置
-		std::cout<<"pos = "<<0<<std::endl;
-		usleep(2187*1000);//[(P1-P0)/V]*1000+[V/(A*100)]*1000
-	}
-	sm_st.end();
-	return 1;
-}
 
+	//csv file
+	fstream fout;
+	fout.open("test1.csv", ios::out | ios::app);
+
+
+
+	sm_st.WritePosEx(1, 4095, 2400, 50); // Start position
+	usleep(2187*1000);//[(P1-P0)/V]*1000+[V/(A*100)]*1000
+	int Pos;
+	int Speed;		
+	int Load;
+	int Time = 0;
+	while(1){
+		sm_st.WritePosEx(1, 4095, 2400, 50); // end position
+		Pos = sm_st.ReadPos(1);
+		Speed = sm_st.ReadSpeed(1);
+		Load = sm_st.ReadLoad(1);	
+		fout << Time << ", "
+             << Load << ", "
+             << Pos << ", "
+             << Speed 
+			 << "\n";
+		usleep(1000);
+		++Time;
+
+
+		
+		
+
+	}
+}
